052a669e48756b679e2157d9545890cd9ba6ea85
Tone Lima <tonefortal@gmail.com>
2016-03-15 18:02:57 -0300

    Implementação básica de login usando Spring Security

--- src/main/java/com/ferasweb/curriculoonline/controller/login/LoginController.java
+++ src/main/java/com/ferasweb/curriculoonline/controller/login/LoginController.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.ferasweb.curriculoonline.controller.login;
 
 import com.ferasweb.curriculoonline.controller.commons.EntityController;
@@ -12,34 +11,67 @@ import com.ferasweb.curriculoonline.exception.EntityException;
 import com.ferasweb.curriculoonline.model.dao.LoginDAO;
 import com.ferasweb.curriculoonline.model.entity.Login;
 import com.ferasweb.curriculoonline.utils.JsfUtil;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  *
  * @author Andressa
  */
 @ManagedBean
-@ViewScoped
-public class LoginController extends EntityController<Login> implements Serializable{
+@SessionScoped
+public class LoginController extends EntityController<Login> implements Serializable {
 
     @EJB
     private LoginDAO loginDao;
-    
+
+    @EJB
+    private FacesContext facesContext;
+
+    @EJB
+    private HttpServletRequest request;
+
+    @EJB
+    private HttpServletResponse response;
+
     private Login current;
 
     public LoginController() {
-        
+
+    }
+    /**
+     * Método reponsável para se autenticar e entrar no sistema
+     * @throws ServletException
+     * @throws IOException 
+     */
+    public void login() throws ServletException, IOException {
+        facesContext = FacesContext.getCurrentInstance();
+        ExternalContext context = facesContext.getExternalContext();
+        request = (HttpServletRequest) context.getRequest();
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/j_spring_security_check");
+        response = (HttpServletResponse) context.getResponse();
+        dispatcher.forward(request, response);
+
+        facesContext.responseComplete();
     }
 
     public Login getCurrent() {
-        if(current == null){
-            
-         current =  getNewEntity();
+        if (current == null) {
+
+            current = getNewEntity();
         }
         return current;
     }
@@ -47,7 +79,7 @@ public class LoginController extends EntityController<Login> implements Serializ
     public void setCurrent(Login current) {
         this.current = current;
     }
-    
+
     @Override
     protected void setEntity(Login t) {
         this.current = t;
@@ -57,8 +89,6 @@ public class LoginController extends EntityController<Login> implements Serializ
     protected Login getNewEntity() {
         return new Login();
     }
-    
-    
 
     @Override
     protected void performDestroy() {
@@ -84,5 +114,5 @@ public class LoginController extends EntityController<Login> implements Serializ
     public EntityPagination getPagination() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
